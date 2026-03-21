078342eeeb3cc7a2c1d285d054a5dd84f96edc40
NoopuraRose <noopura.rose@gmail.com>
2026-03-20 18:31:49 -0700

    data sets

--- height_weight_predictor_knn.py
+++ height_weight_predictor_knn.py
@@ -1,5 +1,7 @@
 import pandas as pd 
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import mean_squared_error
+import numpy as np
 
 mydata = pd.read_csv("data.csv")
 x = mydata[["height"]]
@@ -10,3 +12,9 @@ model.fit(x,y)
 
 new_weight = model.predict([[160]])
 print("Predicted weight = ", new_weight)
+
+y_pred = model.predict(x)
+mse = mean_squared_error(y,y_pred)
+print("MSE = ", mse)
+rmse = np.sqrt(mse)
+print("RMSE = ", rmse)
